14cf363b500d7a873e48bedcff8f86cce47cd81d
peterrobert <pwambui93@gmail.com>
2020-08-05 19:43:36 +0300

    created the video tags and source tags

--- src/home.js
+++ src/home.js
@@ -1,7 +1,60 @@
-class HomeDisplay{
-   
-    
+class HomeDisplay {
+
+    static homePage() {
+        //  attributes for video and source tags
+        const videoAtt = [{
+            'autoplay': ''
+        }, {
+            'muted': ''
+        }, {
+            'loop': ''
+        }, {
+            'id': "myVideo"
+        }];
+        const sourceAtt = [{
+            'src': '../assets/images/video.mp4'
+        }, {
+            'type': 'video/mp4'
+        }]
+
+        // getting the container and creating the video tags
+        let container = document.getElementById('content');
+        let videoTag = document.createElement('video');
+
+        // looping through the video attributes and setting their values
+        let settingAttVideo = (att) => {
+            return videoTag.setAttribute(Object.getOwnPropertyNames(att), Object.values(att));
+        }
+        videoAtt.forEach(settingAttVideo);
+
+        // appending the video tag
+        container.append(videoTag);
+
+        // looping through the source attributes setting their values and appending the values to video tag
+        let sourceTag = document.createElement('source');
+
+        let settingAttSource = (att) => {
+            return sourceTag.setAttribute(Object.getOwnPropertyNames(att), Object.values(att));
+        }
+
+        sourceAtt.forEach(settingAttSource);
+        videoTag.appendChild(sourceTag)
+
+    };
+
+
+    static homeDetails() {
+        let navigation = ["home", "menu", "contacts"];
+
+        let container = document.getElementById('content');
+
+
+    };
+
+
 
 };
 
-export{HomeDisplay};
+export {
+    HomeDisplay
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import { HomeDisplay } from "./home.js";
+
+window.addEventListener("load", HomeDisplay.homePage());
