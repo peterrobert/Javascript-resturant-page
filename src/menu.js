d3005a40f569efde0296da3da3f7c29fb7bc8f98
peterrobert <pwambui93@gmail.com>
2020-08-10 13:42:23 +0300

    used append on the menu page

--- src/menu.js
+++ src/menu.js
@@ -6,16 +6,17 @@ class Menu {
 
     static displayMenu() {
 
+
         let list = MenuItems.showItems();
         let listArr = list.menuList
 
         let container = document.getElementById('content');
 
         let details = document.createElement('div');
-        details.setAttribute('class','home_details');
-        details.setAttribute('id','menu_details');
+        details.setAttribute('class', 'home_details');
+        details.setAttribute('id', 'menu_details');
 
-    
+
         // creating the html structure.
 
         let mainDiv = document.createElement("div");
@@ -23,23 +24,52 @@ class Menu {
 
         let secondaryDiv = document.createElement("div");
         secondaryDiv.setAttribute("class", "row");
-        
-        
+
+        function setAttributes(el, attrs) {
+            for (var key in attrs) {
+                el.setAttribute(key, attrs[key]);
+            }
+        }
 
         for (let i = 0; i < listArr.length; i++) {
-         let  thirdDiv = document.createElement("div");
-             thirdDiv.setAttribute("class", "col-md-3");
 
-            thirdDiv.innerHTML = (`<div class="card">
-            <img src="${list.menuList[i].image}" alt="${list.menuList[i].Name}" style="width:100%">
-            <h1>${list.menuList[i].Name}</h1>
-            <p class="price">$ ${list.menuList[i].price}</p>
-          </div>`)
+            let thirdDiv = document.createElement("div");
+            setAttributes(thirdDiv, {
+                "class": "col-md-3"
+            });
+            console.log(thirdDiv)
 
-          secondaryDiv.append(thirdDiv);
-        } 
+            let card = document.createElement('div');
+            setAttributes(card, {
+                "class": "card"
+            });
 
-       
+            let cardImg = document.createElement('img');
+            setAttributes(cardImg, {
+                "src": `${list.menuList[i].image}`,
+                'alt': `${list.menuList[i].Name}`,
+                "styles": "width:100%"
+            });
+
+            card.append(cardImg);
+
+            let cardH1 = document.createElement('h1');
+            cardH1.innerHTML = `${list.menuList[i].Name}`;
+            card.append(cardH1);
+
+            let cardP = document.createElement('p');
+            setAttributes(cardP, {
+                "class": "price"
+            });
+            cardP.innerText = `${list.menuList[i].price}`
+            card.append(cardP);
+
+            thirdDiv.append(card);
+
+            secondaryDiv.append(thirdDiv);
+        }
+
+
         mainDiv.append(secondaryDiv);
 
         details.append(mainDiv);
