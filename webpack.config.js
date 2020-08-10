edc4a3e0240e2e0cf1427827584b1614cb98b293
peterrobert <pwambui93@gmail.com>
2020-08-10 14:21:15 +0300

    fixed eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
